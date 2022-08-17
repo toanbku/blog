e147d1b2f0d66fd2e30e028c13063fd7328b1c4e
Toan Ho <toanbku@gmail.com>
2022-08-17 21:37:33 +0700

    feat: upgrade astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -9,18 +8,22 @@ export default defineConfig({
   publicDir: "./public",
   outDir: "./dist",
   vite: {
-    plugins: [{
-      name: "import.meta.url-transformer",
-      transform: (code, id) => {
-        if (id.endsWith(".astro")) return code.replace(/import.meta.url/g, `"${id}"`);
-      }
-    }],
+    plugins: [
+      {
+        name: "import.meta.url-transformer",
+        transform: (code, id) => {
+          if (id.endsWith(".astro"))
+            return code.replace(/import.meta.url/g, `"${id}"`);
+        },
+      },
+    ],
     ssr: {
-      external: ["svgo"]
-    }
+      external: ["svgo"],
+      noExternal: ["astro", "@astrojs/image", "astro-imagetools"],
+    },
   },
   experimental: {
-    integrations: true
+    integrations: true,
   },
-  integrations: [preact(), astroImageTools, tailwind()]
+  integrations: [preact(), astroImageTools, tailwind()],
 });
